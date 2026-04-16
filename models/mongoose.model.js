6af18936e8e71094898a256cd1cfdbc1a403734c
khushal <mongakhushal3@gmail.com>
2026-04-16 21:57:23 +0530

    attribute dtype restrictions

--- models/mongoose.model.js
+++ models/mongoose.model.js
@@ -2,8 +2,8 @@ let mongoose=require('mongoose');
 
 let userSchema=mongoose.Schema({
     name:String,
-    roll:{type:String,unique:true},
-    age:{type : String}
+    roll:{type:Number,unique:true},
+    age:{type : Number, min: 10, max: 80}
 });
 let User=mongoose.model('User',userSchema);
 module.exports=User;
