d1087a7eea2f34ab9cd65364c92d23ee9d493e8a
khushal <mongakhushal3@gmail.com>
2026-04-16 09:11:27 +0530

    corss correction again

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let express=require('express');
 let mongoose =require('mongoose');
 let cntrl =require('./routs/ruts');
-const cors = require('cors');
-app.use(cors({origin: 'https://whimsical-mochi-dad659.netlify.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],credentials: true
-}));
+// const cors = require('cors');
+// app.use(cors({origin: 'https://whimsical-mochi-dad659.netlify.app',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],credentials: true
+// }));
 let app=express();
 
-
+const cors = require('cors');
+app.use(cors());
 const bodyParser = require('body-parser'); // to parse JSON
 app.use(bodyParser.json());
 
